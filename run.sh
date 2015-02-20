d50e725bf577cf039ca61bc1e9dad61654280b79
gtt116 <gtt116@gmail.com>
2015-02-20 11:55:14 +0800

    copy js and css to target

--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 ~/src/tiebabot/.venv/bin/python ~/src/tiebabot/tiebabot/main.py
+cp -r -v  ~/src/tiebabot/tiebabot/templates/css  /var/www/tieba/
+cp -r -v  ~/src/tiebabot/tiebabot/templates/js  /var/www/tieba/
